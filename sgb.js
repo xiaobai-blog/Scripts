bc2ad6a018c06944f691eecc0d9a99be1f5a609e
xiaobai <55676895+xiaobai-blog@users.noreply.github.com>
2024-05-25 20:02:07 +0800

    sgb.js

--- sgb.js
+++ sgb.js
@@ -23,7 +23,7 @@ hostname = *fkdzz.bueryx*
 const responseBody = $response.body; 
 const uinMatch = responseBody.match(/"uin":(\d+),/);
 
-if (uinMatch && (uinMatch[1] !== "16914420" && uinMatch[1] !== "11111" && uinMatch[1] !== "49151065" && uinMatch[1] !== "11111" && uinMatch[1] !== "11111")) {
+if (uinMatch && (uinMatch[1] !== "16914420" && uinMatch[1] !== "11111" && uinMatch[1] !== "16914420" && uinMatch[1] !== "11111" && uinMatch[1] !== "11111")) {
 
     $notify("未授权", "添加l648226460授权");
     $done();
